135acfcf0d1dd5ed96c036c3915adae803987222
Denitsa Dobreva <denitsa.dr.dobreva@gmail.com>
2020-08-29 16:38:39 +0300

    update documentation

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const normalize = require("normalize-url");
+const auth = require("../../middleware/auth");
 
 const User = require("../../models/User");
 
